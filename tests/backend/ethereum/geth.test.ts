180d9f7440eb516276a9606226dc97f01564a4cb
Danqing Liu <git@danqing.io>
2018-04-13 00:08:02 +0800

    Add test and absolute file path

---
+++ tests/backend/ethereum/geth.test.ts
@@ -0,0 +1,38 @@
+import test from 'ava';
+
+import { Geth } from '../../../backend/ethereum/geth';
+
+test('start script should contain correct flags', t => {
+	const flags = {
+		nodiscover: true,
+		rpc: true,
+		rpcport: 8546,
+		rpcapi: ['web3', 'eth'],
+		port: 30304,
+		identity: 'boson',
+	};
+	t.deepEqual(Geth.startScript(flags), {
+		command: 'geth',
+		options: [
+			'console',
+			'--nodiscover', '--port', '30304', '--identity', 'boson',
+			'--rpc', '--rpcport', '8546', '--rpcapi', 'web3,eth',
+		]
+	});
+});
+
+test('start script should ignore RPC flags if RPC is not enabled', t => {
+	const flags = {
+		nodiscover: true,
+		rpcport: 8546,
+		rpcapi: ['web3', 'eth'],
+		port: 30304,
+		identity: 'boson',
+	};
+	t.deepEqual(Geth.startScript(flags), {
+		command: 'geth',
+		options: [
+			'console', '--nodiscover', '--port', '30304', '--identity', 'boson',
+		]
+	});
+});
