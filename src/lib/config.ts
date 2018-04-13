3e238b8b448cd26fe7b47262213e0df43c6eaa74
hengchu <hengchu.zhang@gmail.com>
2018-04-13 11:19:08 -0400

    [impl] fleshing out initialization configurations

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,4 +3,36 @@ interface IBosonConfig {
   chain: string;
   // Backend to use. Default is geth.
   backend?: string;
+  // An array of account keystore file paths, this will be used to import
+  // accounts on initialization.
+  accounts: string[];
+  // An array of host IP:port addresses, in that format.
+  hosts: string[];  
+}
+
+/**
+ * For local dev networks, we can control what their parameters are. These can
+ * be used to fill the parameters to IGethFlags, for example, when we start a
+ * local network. But they should be generic enough to support all the backends.
+ * 
+ * TODO[hengchu]: I'm not super sure what the parameters should be yet, but
+ * here's a couple.
+ */
+interface IBosonDevConfig extends IBosonConfig {
+  // Custom node name.
+  nodeName: string;
+  // Network id.
+  networkid: number;
+  // Gas price multiplier (in Wei).
+  gasPrice: number;
+}
+
+/**
+ * For remote networks, we cannot control how they're started. But having a
+ * separate type for live configurations could make code clearer.
+ * 
+ * TODO[hengchu]: I'm not super sure what the parameters should be yet.
+ */
+interface IBosonLiveConfig extends IBosonConfig {
+  // EMPTY
 }
