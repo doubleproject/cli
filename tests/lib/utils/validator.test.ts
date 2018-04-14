f1ed885f73509ee71f1c509200dcde3b54dfd48a
hengchu <hengchu.zhang@gmail.com>
2018-04-13 21:13:12 -0400

    more tests

--- tests/lib/utils/validator.test.ts
+++ tests/lib/utils/validator.test.ts
@@ -47,3 +47,43 @@ test('validator should report empty hosts', async t => {
     t.fail('Validator should report empty host property on root config');
   }
 });
+
+test('empty local config should be valid', async t => {
+  const config = {};
+
+  var validator = new Validator();
+  const result = await validator.validateLocalConfig(config);
+  if (typeof result === 'string') {
+    t.fail(result);
+  } else {
+    t.pass();
+  }
+});
+
+test('local config with empty hosts is invalid', async t => {
+  const config = {
+    hosts: []
+  };
+
+  var validator = new Validator();
+  const result = await validator.validateLocalConfig(config);
+  if (typeof result === 'string') {
+    t.pass(result);
+  } else {
+    t.fail('Validator should reject local config with empty hosts');
+  }
+});
+
+test('local config with hosts is valid', async t => {
+  const config = {
+    hosts: ['127.0.0.1:8080']
+  };
+
+  var validator = new Validator();
+  const result = await validator.validateLocalConfig(config);
+  if (typeof result === 'string') {
+    t.fail(result);
+  } else {
+    t.pass();
+  }
+});
