1fbd1e7a4ae8f080e37b24766acd0b7a3d2e86a6
Danqing Liu <git@danqing.io>
2018-04-23 21:09:27 +0800

    update

--- src/tests/apis/start.test.ts
+++ src/tests/apis/start.test.ts
@@ -1,6 +1,11 @@
 import test from 'ava';
 
 import * as start from '../../apis/start';
+// import run, { DOWN, ENTER } from '../utils/inquirer';
+
+// test('should be able to select from multiple envs', async t => {
+//   await run(['start'], [DOWN, DOWN, ENTER]);
+// });
 
 test('should not be able to start invalid chain', t => {
   t.throws(() => {
