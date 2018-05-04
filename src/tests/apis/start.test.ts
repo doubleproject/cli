6dd4064c259446ff0abc2cc95cd3ed6bffafdd28
Danqing Liu <git@danqing.io>
2018-05-04 14:56:14 -0700

    update test

--- src/tests/apis/start.test.ts
+++ src/tests/apis/start.test.ts
@@ -1,8 +1,10 @@
 import test from 'ava';
+import * as inquirer from 'inquirer';
+import * as sinon from 'sinon';
 
 import * as start from '../../apis/start';
+import * as ethereum from '../../backend/ethereum';
 import Config from '../../config';
-// import run, { DOWN, ENTER } from '../utils/inquirer';
 
 test('should not be able to start invalid environment', t => {
   t.throws(() => {
@@ -24,9 +26,18 @@ test('should fail for config without local environment', t => {
   });
 });
 
-// test('should be able to select from multiple envs', async t => {
-//   await run(['start'], [DOWN, DOWN, ENTER]);
-// });
+test('should prompt user to choose from multiple environments', t => {
+  const cfg = Config.parseFromFile('data/tests/multi.yaml');
+  sinon.stub(ethereum, 'start');
+  sinon.stub(inquirer, 'prompt').resolves({env: 'local1'});
+  start.startForConfig(cfg);
+  t.truthy((inquirer.prompt as sinon.SinonStub).calledWithMatch([{
+    type: 'list',
+    name: 'env',
+    message: 'Select the local environment to start:',
+    choices: ['local1', 'local2', 'local3'],
+  }]));
+});
 
 test('should not be able to start invalid chain', t => {
   t.throws(() => {
